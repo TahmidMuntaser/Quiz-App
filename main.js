9eaf9dc939f0870f2c32288ef59eb64935b8df94
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-04-15 14:39:56 +0600

    upd-0.4

---
+++ main.js
@@ -0,0 +1,60 @@
+const questions = [
+    {
+        question: "What does the abbreviation HTML stand for?",
+        answer: [
+            { text: "HighTech Markup Language", correct: false },
+            { text: "HyperText Machine Language", correct: false },
+            { text: "HyperText Markup Language", correct: true },
+            { text: "HighTech Machine Language", correct: false }
+        ]
+
+    },
+
+    {
+        question: "How many sizes of headers are available in HTML by default?",
+        answer: [
+            { text: "2", correct: false },
+            { text: "4", correct: false },
+            { text: "5", correct: false },
+            { text: "6", correct: true }
+        ]
+
+    },
+
+    {
+        question: "How to create an ordered list in HTML?",
+        answer: [
+            { text: "<ul>", correct: false },
+            { text: "<ol>", correct: true },
+            { text: "<li>", correct: false },
+            { text: "<href>", correct: false }
+        ]
+
+    },
+
+    {
+        question: "We enclose HTML tags within?",
+        answer: [
+            { text: "<>", correct: true },
+            { text: "/", correct: false },
+            { text: "{}", correct: false },
+            { text: "None of these", correct: false }
+        ]
+
+    },
+
+    {
+        question: "Which of the following is correct about HTML?",
+        answer: [
+            { text: "HTML uses user-defined Tags", correct: false },
+            { text: "HTML uses tags defined within the language", correct: true },
+            { text: "Both A and B", correct: false },
+            { text: "None of theseg", correct: false }
+        ]
+
+    },
+
+
+];
+
+

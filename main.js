bb1edcbf92af9b32b7791a3134ea0044bb719b3a
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-04-16 21:41:43 +0600

    suffle

--- main.js
+++ main.js
@@ -56,6 +56,15 @@ const questions = [
 
 ];
 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+shuffle(questions);
+
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answerbtn");
 const nextButton = document.getElementById("nxtbtn");
